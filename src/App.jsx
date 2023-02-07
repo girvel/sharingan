fbbd991bb65635e1b96affd4aedf8da9b640b653
girvel <widauka@ya.ru>
2023-02-07 15:35:56 +0400

    Hello world

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import './App.css'
 
 function App() {
   return (
-    <></>
-  )
+    <>
+      <p>Hello world!</p>
+      <p>How's it going?</p>
+    </>
+  );
 }
 
-export default App
+export default App;
