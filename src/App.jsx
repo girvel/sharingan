8dc7a3f26ce9dfe7aa912291779b19ff133160e9
girvel <widauka@ya.ru>
2023-02-07 18:31:03 +0400

    h2

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import './App.css'
 function App() {
   return (
     <>
+      <h2>Header</h2>
       <p>Hello world!</p>
       <p>How's it going?</p>
+      <div>a</div>
+      <div>a</div>
     </>
   );
 }
