dda07e1df44acdb5cdbf6a09a383de4d5d65e174
girvel <widauka@ya.ru>
2023-02-09 11:18:57 +0400

    Add a dataclass for groupDataBySkills

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
 import './App.css'
 import Skill from "./components/Skill"
-import {group} from "./toolkit/statistics.js";
 import {useState} from "react";
-import {fetchDataFromDb} from "./toolkit/db.js";
+import {fetchDataFromDb, groupDataBySkills} from "./toolkit/db.js";
 
-
-// should return [{exercise, levels, user_level, sets}]
-function groupDataBySkills(data) {
-  const sets_grouped = group(data.sets, s => s.exercise);
-  return Array.from(group(data.all_levels, l => l.exercise)).map(([exercise, levels]) => {
-    return {
-      exercise: exercise,
-      levels: levels,
-      user_level: data.user_levels.find(ul => ul.exercise === exercise)?.level ?? 1,
-      sets: sets_grouped.get(exercise) ?? [],
-    };
-  });
-}
 
 let grouped_skill_data = groupDataBySkills(fetchDataFromDb());
 
--- src/toolkit/db.js
+++ src/toolkit/db.js
@@ -1,3 +1,6 @@
+import {group} from "./statistics.js";
+import {Data} from "dataclass";
+
 export function fetchDataFromDb() {
   const sets = [
     {exercise: "Push-ups", amount: 10, level: 1},
@@ -13,4 +16,16 @@ export function fetchDataFromDb() {
   ]
 
   return {sets: sets, all_levels: all_levels, user_levels: user_levels};
+}
+
+class SkillData extends Data { exercise; levels; user_level; sets; }
+
+export function groupDataBySkills(data) {
+  const sets_grouped = group(data.sets, s => s.exercise);
+  return Array.from(group(data.all_levels, l => l.exercise)).map(([exercise, levels]) => SkillData.create({
+    exercise: exercise,
+    levels: levels,
+    user_level: data.user_levels.find(ul => ul.exercise === exercise)?.level ?? 1,
+    sets: sets_grouped.get(exercise) ?? [],
+  }));
 }
