6ee2e278d688ade20593ee4437d307d2d7ebc73d
girvel <widauka@ya.ru>
2023-02-12 10:09:11 +0400

    Fully connect DB

--- src/toolkit/db.js
+++ src/toolkit/db.js
@@ -1,40 +1,31 @@
 import {group} from "./statistics.js";
 import {Data} from "dataclass";
 
+class DbRawData extends Data { levels; sets; user_levels; }
+
 export async function fetchDataFromDb() {
-  const sets = [
-    {exercise: "Push-ups", amount: 10, level: 1},
-  ];
+  const response = await fetch("http://localhost:8000/user_data/", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      user: "girvel",
+    }),
+  });
 
-  // const all_levels = [
-  //   {exercise: "Push-ups", level: 1, level_name: "Normal", limit: 30},
-  //   {exercise: "Leg raises", level: 1, level_name: "Knee raises", limit: 30},
-  // ];
-
-  const response = await fetch("http://localhost:8000/exercise_levels")
-  console.log(response);
-
-  let all_levels;
   if (response.ok) {
-    all_levels = (await response.json())['exercises'];
-    console.log(all_levels);
-  }
-  else {
-    console.error(`HTTP error ${response.status}: ${response.statusText}`);
+    return DbRawData.create(await response.json());
   }
 
-  const user_levels = [
-    {exercise: "Push-ups", level: 1},
-  ]
-
-  return {sets: sets, all_levels: all_levels, user_levels: user_levels};
+  console.error(`HTTP error ${response.status}: ${response.statusText}`);
 }
 
 class SkillData extends Data { exercise; levels; user_level; sets; }
 
 export function groupDataBySkills(data) {
   const sets_grouped = group(data.sets, s => s.exercise);
-  return Array.from(group(data.all_levels, l => l.exercise)).map(([exercise, levels]) => SkillData.create({
+  return Array.from(group(data.levels, l => l.exercise)).map(([exercise, levels]) => SkillData.create({
     exercise: exercise,
     levels: levels,
     user_level: data.user_levels.find(ul => ul.exercise === exercise)?.level ?? 1,
