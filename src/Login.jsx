5e41205b94e7410a347ba1d4837c71cdbd6a60fe
girvel <widauka@ya.ru>
2023-02-14 19:42:55 +0400

    Stylize the login component

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,18 @@
+import './Login.css';
+
 export default function Login({onLogin}) {
   // TODO forms
   function onSubmit() {
-    onLogin(document.getElementById("username_input").value)
+    onLogin(document.getElementById("username_input").value);
   }
 
   return (
-    <form>
-      <input id="username_input"></input>
-      <button onClick={onSubmit}>Login</button>
-    </form>
+    <>
+      <h1>Sharingan</h1>
+      <form className="login_form">
+        <input id="username_input" maxLength="40" placeholder="Username" className="login_element"></input>
+        <button onClick={onSubmit} className="login_element">Login</button>
+      </form>
+    </>
   )
 }
