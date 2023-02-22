0c14f48687195f69c3c83ae4f67a10c46ed3f087
girvel <widauka@ya.ru>
2023-02-22 11:03:49 +0400

    Extract remote_call in db.js

--- src/toolkit/db.js
+++ src/toolkit/db.js
@@ -4,33 +4,30 @@ import {inDevelopmentMode} from "./stuff.js";
 
 const server_url = inDevelopmentMode() ? "http://localhost:8000" : "http://aws.girvel.xyz";
 
-async function post(call, args) {
-  const response = await fetch(`${server_url}/${call}/`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(args),
-  });
+async function remote_call(call, parameters) {
+  const response = await fetch(`${server_url}/${call}`, parameters);
 
   if (response.ok) {
-    return await response.json();
+    return await response.json()
   }
 
   console.error(`HTTP error ${response.status}: ${response.statusText}`);
   return Promise.reject();
 }
 
+async function post(call, args) {
+  return remote_call(call, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(args),
+  });
+}
+
 // TODO extract common logic
 async function get(call, args) {
-  const response = await fetch(`${server_url}/${call}?${new URLSearchParams(args)}`, { method: 'GET' });
-
-  if (response.ok) {
-    return await response.json();
-  }
-
-  console.error(`HTTP error ${response.status}: ${response.statusText}`);
-  return Promise.reject();
+  return remote_call(`${call}?${new URLSearchParams(args)}`, { method: 'GET' });
 }
 
 class DbRawData extends Data { levels; sets; user_levels; }
